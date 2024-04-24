761865353941755564ae70435c227d16629c18d3
Santiago Quintero <86493587+santiqc@users.noreply.github.com>
2024-04-23 23:18:04 -0500

    se inyecta httpClientModule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,8 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
