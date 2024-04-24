76907330b047d6dfd51e75e659bdb1d70e5b5e77
Santiago Quintero <86493587+santiqc@users.noreply.github.com>
2024-04-24 08:06:39 -0500

    se agrega alertService

---
+++ src/app/components/services/alertService.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import Swal from 'sweetalert2';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AlertService {
+
+  constructor() { }
+
+  public mensajeExito(text: string): void {
+    Swal.fire({
+      title: "Informacion",
+      text: text,
+      icon: "success",
+      confirmButtonColor: "#62e462",
+    });
+  }
+
+  public mensajeConfirmacion(text: string): Promise<any> {
+    return Swal.fire({
+      title: "Informacion",
+      text: text,
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#62e462",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Aceptar!"
+    }).then((result) => {
+      return result;
+    });
+  }
+
+
+}
